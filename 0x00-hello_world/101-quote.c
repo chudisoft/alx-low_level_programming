7da478433a8cda22bdc7792763e8939fce9778ee
chudisoft <christopheruduekwe@gmail.com>
2023-03-16 15:17:56 -0700

    Completed All

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A C program that prints a line to the standard error
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,2 @@
-0-preprocessor in __Hello World Project__
+#!/bin/bash
+Write a script that runs a C file through the preprocessor and save the result into another file.

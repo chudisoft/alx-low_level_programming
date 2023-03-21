806e3f89dc09c7cf96f22305ed07bc4b1b17adc9
chudisoft <christopheruduekwe@gmail.com>
2023-03-21 12:51:08 -0700

    Added and pushing all

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -3,7 +3,7 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *
+ * 
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */

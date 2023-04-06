cf8847380d8adaf0fad5410d776519295f6b65fb
chudisoft <christopheruduekwe@gmail.com>
2023-04-05 22:06:53 -0700

    Added _putchar function for print

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -7,3 +10,6 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int _putchar(*s);
+
+#endif
